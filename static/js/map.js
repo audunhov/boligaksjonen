0aabbedfa46aa3400a016e976591bd6fa1e40e12
Audun Hammer Hovda <hammerhovda@gmail.com>
2026-03-23 23:38:00 +0100

    style: replace default map markers with custom brand-blue SVG pins

--- static/js/map.js
+++ static/js/map.js
@@ -349,11 +349,23 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(res => res.json())
         .then(houses => {
             allHouses = houses;
+            
+            const customIcon = L.divIcon({
+                className: 'custom-div-icon',
+                html: `
+                    <svg class="size-8 drop-shadow-lg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C8.13 2 5 5.13 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 5.13 15.87 2 12 2ZM12 11.5C10.62 11.5 9.5 10.38 9.5 9C9.5 7.62 10.62 6.5 12 6.5C13.38 6.5 14.5 7.62 14.5 9C14.5 10.38 13.38 11.5 12 11.5Z" fill="#2563EB"/>
+                    </svg>
+                `,
+                iconSize: [32, 32],
+                iconAnchor: [16, 32]
+            });
+
             houses.forEach(h => {
                 // Only show active houses on the map
                 if (h.is_deleted) return;
 
-                const m = L.marker([h.lat, h.lng]);
+                const m = L.marker([h.lat, h.lng], { icon: customIcon });
                 m.on('click', () => {
                     map.flyTo([h.lat, h.lng], 17);
                     window.showHouseDetails(h);
